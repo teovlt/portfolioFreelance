9bf049ca3777f863b7401bc9be3c792d61298cce
teovlt <teo.villet2@gmail.com>
2025-05-22 23:21:53 +0200

    Add experience page

---
+++ src/pages/Experiences.tsx
@@ -0,0 +1,7 @@
+export const Experiences = () => {
+  return (
+    <section className="min-h-screen flex items-center justify-center p-4" id="experiences-section">
+      <div className="container">Hello from Experiences</div>
+    </section>
+  );
+};
