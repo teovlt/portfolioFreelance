7fa17fcfc7a13e4c16ed1bf34622334ef64fa4a1
teovlt <teo.villet2@gmail.com>
2025-06-24 11:40:15 +0200

    Fix speed insights for vercel

--- src/main.tsx
+++ src/main.tsx
@@ -13,6 +13,7 @@ createRoot(document.getElementById("root")!).render(
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <RouterProvider router={router} />
       <Toaster />
+      <SpeedInsights />
     </ThemeProvider>
   </StrictMode>,
 );
