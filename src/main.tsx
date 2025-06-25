be51702b314ac3e74870df121166226ffd79009a
teovlt <teo.villet2@gmail.com>
2025-06-25 07:11:31 +0200

    Delete forgotten star

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,6 @@ import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    *{" "}
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <RouterProvider router={router} />
       <Toaster />
