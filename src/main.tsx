0eb3438836807df4d84f2d487f2e73dc43ecabd7
teovlt <teo.villet2@gmail.com>
2025-06-30 16:53:32 +0200

    Try to improve name and desc on google

--- src/main.tsx
+++ src/main.tsx
@@ -8,10 +8,18 @@ import "./lib/i18n";
 import { Toaster } from "./components/ui/sonner";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import { Analytics } from "@vercel/analytics/react";
+import { Helmet } from "react-helmet";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Helmet>
+        <meta
+          title="Téo Villet - Développeur Freelance à Grenoble - Portfolio"
+          name="description"
+          content="Je suis Téo, développeur fullstack basé à Grenoble. Voici mon portfolio : projets, stack technique, expériences pro et contact rapide. N'hésitez pas à me contacter pour toute question ou proposition de collaboration !"
+        />
+      </Helmet>
       <RouterProvider router={router} />
       <Toaster />
       <SpeedInsights />
