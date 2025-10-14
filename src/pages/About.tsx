a4a83e20c7c50bbee48e034bdf28f43bfd6502b9
teovlt <teo.villet2@gmail.com>
2025-10-14 14:58:19 +0200

    Merci gemini

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -85,7 +85,7 @@ export const About = () => {
                 >
                   <Avatar className="w-full h-full shadow-lg">
                     <AvatarImage
-                      src="images/me.jpg"
+                      src="images/gemini.png"
                       alt="Photo de Téo Villet, développeur web freelance à Grenoble"
                       className="object-cover w-full h-full"
                     />
